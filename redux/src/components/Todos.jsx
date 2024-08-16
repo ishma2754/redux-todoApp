f1d83161c05b43e66e4cbcf8fd929894797e4124
Ishma <ishmakhan1995@gmail.com>
2024-08-16 18:25:51 +0530

    todo app with redux

---
+++ redux/src/components/Todos.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo} from "../features/todo/todoSlice";
+
+function Todos({setInput}) {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+
+  return (
+    <>
+      <div>Todos</div>
+      <ul className="list-none">
+        {todos.map((todo) => (
+          <li
+            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+            key={todo.id}
+          >
+            <div className="text-white">{todo.text}</div>
+            <button
+             
+              className="text-white bg-orange-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                viewBox="0 0 50 50"
+                className="w-6 h-6"
+              >
+                <path d="M 43.125 2 C 41.878906 2 40.636719 2.488281 39.6875 3.4375 L 38.875 4.25 L 45.75 11.125 C 45.746094 11.128906 46.5625 10.3125 46.5625 10.3125 C 48.464844 8.410156 48.460938 5.335938 46.5625 3.4375 C 45.609375 2.488281 44.371094 2 43.125 2 Z M 37.34375 6.03125 C 37.117188 6.0625 36.90625 6.175781 36.75 6.34375 L 4.3125 38.8125 C 4.183594 38.929688 4.085938 39.082031 4.03125 39.25 L 2.03125 46.75 C 1.941406 47.09375 2.042969 47.457031 2.292969 47.707031 C 2.542969 47.957031 2.90625 48.058594 3.25 47.96875 L 10.75 45.96875 C 10.917969 45.914063 11.070313 45.816406 11.1875 45.6875 L 43.65625 13.25 C 44.054688 12.863281 44.058594 12.226563 43.671875 11.828125 C 43.285156 11.429688 42.648438 11.425781 42.25 11.8125 L 9.96875 44.09375 L 5.90625 40.03125 L 38.1875 7.75 C 38.488281 7.460938 38.578125 7.011719 38.410156 6.628906 C 38.242188 6.246094 37.855469 6.007813 37.4375 6.03125 C 37.40625 6.03125 37.375 6.03125 37.34375 6.03125 Z"></path>
+              </svg>
+            </button>
+            <button
+              onClick={() => dispatch(removeTodo(todo.id))}
+              className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                />
+              </svg>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Todos;
